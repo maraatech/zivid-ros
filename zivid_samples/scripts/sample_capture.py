98acaceb1f8d99269e1318b6bdcb66ce249c495c
Yonghui <tqia574@aucklanduni.ac.nz>
2020-05-19 13:03:51 +1200

    update zivid-ros sampling and urdf

--- zivid_samples/scripts/sample_capture.py
+++ zivid_samples/scripts/sample_capture.py
@@ -14,22 +14,22 @@ class Sample:
 
         rospy.loginfo("Starting sample_capture.py")
 
-        rospy.wait_for_service("/camera/zivid_camera/capture", 30.0)
+        rospy.wait_for_service("/zivid_camera/capture", 30.0)
 
-        rospy.Subscriber("/camera/zivid_camera/points", PointCloud2, self.on_points)
+        rospy.Subscriber("/zivid_camera/points", PointCloud2, self.on_points)
 
-        self.capture_service = rospy.ServiceProxy("/camera/zivid_camera/capture", Capture)
+        self.capture_service = rospy.ServiceProxy("/zivid_camera/capture", Capture)
 
         rospy.loginfo("Enabling the reflection filter")
         general_config_client = dynamic_reconfigure.client.Client(
-            "/camera/zivid_camera/capture/general/"
+            "/zivid_camera/capture/general/"
         )
         general_config = {"filters_reflection_enabled": True}
         general_config_client.update_configuration(general_config)
 
         rospy.loginfo("Enabling and configure the first frame")
         frame0_config_client = dynamic_reconfigure.client.Client(
-            "/camera/zivid_camera/capture/frame_0"
+            "/zivid_camera/capture/frame_0"
         )
         frame0_config = {"enabled": True, "iris": 21, "exposure_time": 20000}
         frame0_config_client.update_configuration(frame0_config)
